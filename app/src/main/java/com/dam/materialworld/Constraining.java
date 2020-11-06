5f3ca9838257b383352bb6310ae8d37da3e92a43
pablosc93 <pablo29.sanchezc@gmail.com>
2020-11-06 11:10:01 +0100

    initial commit

---
+++ app/src/main/java/com/dam/materialworld/Constraining.java
@@ -0,0 +1,14 @@
+package com.dam.materialworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Constraining extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraining);
+    }
+}
